962e95bbe34463850c48848d112cf9c921c0f8c9
Pavel Karanda <77533722+karanda-p@users.noreply.github.com>
2021-04-22 17:34:29 +0300

    Update

--- src/main/java/ru/itfbgroup/notificationservice/service/NotificationService.java
+++ src/main/java/ru/itfbgroup/notificationservice/service/NotificationService.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import ru.itfbgroup.notificationservice.model.Customer;
 import ru.itfbgroup.notificationservice.model.NotificationMessage;
-import ru.itfbgroup.notificationservice.model.Order;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +12,15 @@ public class NotificationService {
 
     private final JavaMailSender mailSender;
 
-    public void sendNotificationMessage(NotificationMessage message){
+    public void sendNotificationMessage(NotificationMessage message) {
 
-        System.out.println(message);
+//        System.out.println(message);
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(message.getEmail());
         msg.setSubject("Смена статуса заказа");
 
         msg.setText(message.getCustomerName() + " статус заказа №" + message.getOrderId() + " изменен на "
-                + message.getOrderStatus().getValue() +"\nс уважением, ваша служба доставки!");
+                + message.getOrderStatus().getValue() + "\nC уважением, ваша служба доставки!");
 
         mailSender.send(msg);
     }
