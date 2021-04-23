1651d5cccbe187476a4313c6c0053c573f297a7b
Pavel Karanda <77533722+karanda-p@users.noreply.github.com>
2021-04-23 18:26:46 +0300

    Update NotificationService.java

--- src/main/java/ru/itfbgroup/notificationservice/service/NotificationService.java
+++ src/main/java/ru/itfbgroup/notificationservice/service/NotificationService.java
@@ -1,11 +1,16 @@
 package ru.itfbgroup.notificationservice.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import ru.itfbgroup.notificationservice.component.TemplateProperty;
+import ru.itfbgroup.notificationservice.model.Attachment;
 import ru.itfbgroup.notificationservice.model.NotificationMessage;
+
+import javax.mail.internet.MimeMessage;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,18 +19,22 @@ public class NotificationService {
     private final JavaMailSender mailSender;
     private final TemplateProperty templateProperty;
 
-    public void sendNotificationMessage(NotificationMessage message) {
+    public void sendNotificationMessage(NotificationMessage message) throws Exception {
 
         TemplateProperty.Template template = templateProperty.getTemplates().get(message.getTemplate());
+        List<Attachment> attachments = message.getAttachments();
+        Map<String, Object> attributes = message.getAttributes();
+        MimeMessage msg = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 //        MimeMailMessage
 //        System.out.println(message);
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(message.getEmail());
-        msg.setSubject("Смена статуса заказа");
+//        SimpleMailMessage msg = new SimpleMailMessage();
+//        msg.setTo(message.getEmail());
+//        msg.setSubject("Смена статуса заказа");
+//
+//        msg.setText(message.getCustomerName() + " статус заказа №" + message.getOrderId() + " изменен на "
+//                + message.getOrderStatus().getValue() + "\nC уважением, ваша служба доставки!");
 
-        msg.setText(message.getCustomerName() + " статус заказа №" + message.getOrderId() + " изменен на "
-                + message.getOrderStatus().getValue() + "\nC уважением, ваша служба доставки!");
-
-        mailSender.send(msg);
+//        mailSender.send(null);
     }
 }
